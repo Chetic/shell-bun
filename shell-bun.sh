581195369ed9678cb8ec34e3108083874eaeebf7
GitHub Action <action@github.com>
2025-06-04 15:16:35 +0000

    Bump version to v1.2

--- shell-bun.sh
+++ shell-bun.sh
@@ -2,7 +2,7 @@
 
 #
 # Shell-Bun - Interactive build environment script
-# Version: 1.1
+# Version: 1.2
 # Copyright (c) 2025, Fredrik Reveny
 # All rights reserved.
 #
@@ -33,7 +33,7 @@
 #
 
 # Version information
-VERSION="1.1"
+VERSION="1.2"
 
 # Shell-Bun - Interactive build environment script
 # Usage: ./shell-bun.sh [config-file]
